a3800cea345f32cab165333292efdb49214e262e
sgh <esghaier.ainformation@gmail.com>
2026-09-24 15:49:52 +0100

    Add import-donnees.js for Excel data import

---
+++ admin/import-donnees.js
@@ -0,0 +1,9 @@
+// =====================================================
+// PLANIFICATION DES EXAMENS FSGF
+// IMPORT-DONNEES.JS
+// Import des données Excel vers Firestore
+// =====================================================
+
+console.log(
+"Import-donnees.js : fichier chargé."
+);
